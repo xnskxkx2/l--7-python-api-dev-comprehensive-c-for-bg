6ad30c55736253aaeb537dbd347f18316c78d890
xnskxkx2 <dimazhartun454@gmail.com>
2026-03-20 01:53:15 +0200

    build: add heroku deployment files (Procfile, runtime, requirements)

--- app/main.py
+++ app/main.py
@@ -9,7 +9,15 @@ from .routers import auth, post, user, vote
 
 app = FastAPI()
 
+origins = ["https://www.google.com"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
